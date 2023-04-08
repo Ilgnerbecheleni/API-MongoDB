2f000137773a9820dc28bdd88c3833c0344ab6f8
ilgner becheleni <ilgner.gui@gmail.com>
2023-04-08 19:06:53 -0300

    up

--- src/app.js
+++ src/app.js
@@ -24,6 +24,14 @@ const customerRoute = require('../routes/Customer');
 const orderRoute = require('../routes/Order');
 
 
+// Habilita o CORS
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
+
 //app
 app.use('/', index);
 app.use('/products', productRoute);
